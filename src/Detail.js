3401c0ed251ea431a258cc8f9af9c014d3ca5f1b
soyoon <sytkfkd96@gmail.com>
2022-01-25 19:13:48 +0900

    기능 구현 완료

--- src/Detail.js
+++ src/Detail.js
@@ -1,13 +1,42 @@
 import React from "react";
-import {useHistory} from "react-router-dom";
-import Home from './Home';
-import {useParams } from "react-router-dom"
+import { useHistory } from "react-router-dom";
+import { useParams } from "react-router-dom"
+import styled from "styled-components";
+
 
 const Detail = (props) => {
     const history = useHistory();
     const day_lists = props.list
     const day_name = useParams();
-//switch문 써서 '내 일주일은?' 삭제하기
+    const rating = React.useRef(null);
+
+    const array = [0,1,2,3,4];
+    // const array2 = [0,1,2,3,4];
+    
+    const [count, setCount] = React.useState();
+    const circle_count = Array.from({ length:count }, (v, i) => i);
+    // const ClickRating = (index) => {
+
+    //     const [clicked, setClicked] = useState([false, false, false, false, false]);
+    //     const targetCircle = [...clicked]
+        
+    //     for(let i=0; i<5; i++) {
+    //         targetCircle[i] <= index? true:false;
+    //     }
+    //     setClicked(targetCircle);
+
+    //     // clickedCircle.current.style.backgroundColor = "yellow"  
+    // }
+
+    // React.useEffect(() => {
+    //     clickedCircle.current.addEventListener("click", clickEvent)
+        
+
+// }, []);
+
+    // window.setTimeout(()=>{},1000);
+    // console.log(rating);
+
     return (
         <>
         <div className="detailSubTitle">
@@ -18,17 +47,45 @@ const Detail = (props) => {
                 color: "white",
             }}>{day_name.index}요일</span> 평점 남기기
         </div>
-        <div className="weekSummary">
-            <div className="makeCircle"
-            //     onClick={() => {
-                
+        <div ref={rating} className="weekSummary">
+            {array.map((el, i)=> {
+                    return (
+                    <div className="makeCircle"
+                    style={{
+                        background:(i<count+1)?"yellow":"gray"
+                    }}
+                    key={i} id={i}
+                    onClick={()=> {
+                        setCount(i)
+                    }}></div>
+                        // const target_circle = document.getElementsByClassName('makeCircle');
+                        // const target_id = target_circle[i].getAttribute('id')
+                        
+                        // array2.map((el, idx)=> {
+                        //     return (
+                        //         (idx <= target_id) ? (target_circle[i].className = "yellowCircle") : (target_circle[i].className = "makeCircle")
+                        //     )
+                        //   } )
+                        
+                        // // target_circle.className = "yellowCircle"
+                        // console.log(target_circle[i].className)
+                        // console.log(target_id)
+                        // console.log(target_circle)
+                        // console.log(target_circle.className)
+                        
+                        // for (let i=0; i<5; i++) {
+                        //     (i <= target_id) ? 노란색 : 회색
+                        // }
+                        
 
+                    
+                    )
+                } )}
 
-            // }}></div>
+            {/* <div className="makeCircle"></div>
             <div className="makeCircle"></div>
             <div className="makeCircle"></div>
-            <div className="makeCircle"></div>
-            <div className="makeCircle"></div>
+            <div className="makeCircle"></div> */}
         </div>
         <div>
             <button className="btnStyle" onClick={()=>{
@@ -38,6 +95,13 @@ const Detail = (props) => {
         </>
     )
 }
+
+// const rating = styled.div `
+	
+// 	background-color: ${(props)=>(props.bg_color? "yello" : "#ddd")};
+// `;
+
+
 
 
 // 서브 연결하는거 - 버킷리스트 map/push 부분 참고
--- src/Home.js
+++ src/Home.js
@@ -4,23 +4,34 @@ import {useHistory} from "react-router-dom";
 const Home = (props) => {
 const history = useHistory();
 const day_lists = props.list
-console.log(day_lists)
+const array = [0,1,2,3,4];
 
 const circle_count = Array.from({ length: 7 }, (v, i) => i);
 
-  // 반환할 리액트 요소가 없을 때는 null을 넘겨주세요!
+
   return (
     <div>
       <h2>내 일주일은?</h2>
       {circle_count.map((num, idx) => {
+        const randomNum = Math.floor(Math.random() * 5)
         return (
             <div className="weekSummary" key={idx}>
               <p>{day_lists[idx]}</p>
+              
+              {array.map((el, i)=> {
+                return (
+                  <div className="makeCircle" 
+                  style={{
+                    backgroundColor:(i<=randomNum)?"yellow":"gray"
+                  }}
+                  key={i}></div>
+                )
+              } )}
+{/*            
               <div className="makeCircle"></div>
               <div className="makeCircle"></div>
               <div className="makeCircle"></div>
-              <div className="makeCircle"></div>
-              <div className="makeCircle"></div>
+              <div className="makeCircle"></div> */}
               <div className="triangle" onClick={()=>{
                 history.push('/detail/' + day_lists[idx]);
               }}></div>
@@ -35,5 +46,7 @@ const circle_count = Array.from({ length: 7 }, (v, i) => i);
 };
 
 
+
+// 랜덤 const params = useParams(); Math.floor(Math.random() * (max - min) + min);
 
 export default Home;
